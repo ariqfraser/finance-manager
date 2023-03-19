1bd2fcc81580a2c171b2c1ac803e157a965d40f3
Ariq Fraser <ariqfraser@gmail.com>
2023-03-19 00:35:23 +0000

    header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  
+  dateToday: Date  = new Date();
+  
+  ngOnInit(): void {
+  }
 }
